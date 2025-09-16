69a292269134d9fa5c89e8ecee8e54eae8edbaea
Darlon Vasata <darlonv@gmail.com>
2025-09-15 21:36:14 -0300

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, render_template, request, after_this_request
+from flask import Flask, Response, render_template
 import os, time, threading
 
 app = Flask(__name__)
@@ -42,4 +42,3 @@ if __name__ == "__main__":
         app.run(host="0.0.0.0", port=8000)
     finally:
         _running = False
-
